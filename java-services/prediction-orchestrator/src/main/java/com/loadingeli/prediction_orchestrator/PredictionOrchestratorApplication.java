507dabc16ff80b72d36f618446b5569df5f4a21d
LoadingElii <delijhiabrown@gmail.comgit config --global user.nam delijhiabrown@gmail.comgit config --global user.email delijhiabrown@gmail.comgit config --global user.email delijhiabrown@gmail.com>
2025-08-05 22:03:17 -0500

    initialized game-predictor architecture

---
+++ java-services/prediction-orchestrator/src/main/java/com/loadingeli/prediction_orchestrator/PredictionOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.loadingeli.prediction_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionOrchestratorApplication.class, args);
+	}
+
+}
