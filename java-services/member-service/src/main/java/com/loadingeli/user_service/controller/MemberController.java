b738b5e30565d813b251079150306ce73eaa45eb
LoadingElii <delijhiabrown@gmail.comgit config --global user.nam delijhiabrown@gmail.comgit config --global user.email delijhiabrown@gmail.comgit config --global user.email delijhiabrown@gmail.com>
2025-08-15 22:15:00 -0500

    rename user_service to member_service

--- java-services/member-service/src/main/java/com/loadingeli/user_service/controller/MemberController.java
+++ java-services/member-service/src/main/java/com/loadingeli/user_service/controller/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/member")
+@RequestMapping("/api/v1/member")
 public class MemberController {
     private final MemberService memberService;
 
